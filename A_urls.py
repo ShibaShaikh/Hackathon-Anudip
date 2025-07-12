d83da0f2c0f8ac03caefa4056fa347394ef398f0
Shiba Shaikh <128081803+ShibaShaikh@users.noreply.github.com>
2025-07-12 10:22:59 +0530

    Delete A_urls.py

--- A_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('vp-allocates/', views.vp_allocates_rm, name="n_vp_allocates"),
-    path('rm-allocates/', views.rm_allocates_center, name="n_rm_allocates"),
-]
