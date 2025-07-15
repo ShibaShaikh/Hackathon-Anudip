79cb3f534380c7d236d2c518bdeb5164e25290e1
Shiba Shaikh <128081803+ShibaShaikh@users.noreply.github.com>
2025-07-15 19:42:35 +0530

    Update models.py

--- PHR/allocation/models.py
+++ PHR/allocation/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from projects.models import CreateProject, VertexAllocation  # PMO-created projects
 from django.conf import settings
+from batches.models import AssignCenter
 
 # vp allocated target to rm: Target breakdown is RM-wise for each vertex
 class VpRmAllocation(models.Model):
@@ -21,7 +22,7 @@ class VpRmAllocation(models.Model):
 class RmCenterQuarterAllocation(models.Model):
     project = models.ForeignKey(CreateProject, on_delete=models.CASCADE)
     vertex = models.CharField(max_length=50, choices=VertexAllocation.VERTEX_CHOICES)
-    center = models.CharField(max_length=50)  # could be a centre model
+    center = models.ForeignKey(AssignCenter, on_delete=models.CASCADE)  # could be a centre model
     quarter = models.CharField(max_length=2, choices=[('Q1','Q1'),('Q2','Q2'),('Q3','Q3'),('Q4','Q4')])
     target = models.PositiveIntegerField()
     rm_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
